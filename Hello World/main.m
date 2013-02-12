fe86621faa68b14cf2a06356966b7867a4d356f3
Maira <Amer@unknown7cd1c38e8d96.gateway.2wire.net>
2013-02-12 10:28:44 -0800

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Maira on 2/12/13.
+//  Copyright (c) 2013 Maira. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
